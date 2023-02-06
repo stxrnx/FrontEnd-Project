ec4d5dc58ad9f2daa0f69868c4fcbfec76bf10fe
stxrna <eric.starnini@gmail.com>
2023-02-06 18:10:32 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './routing.module';
+
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
